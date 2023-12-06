8b039ab8c9537999b36272911feb896539a43b4e
Michael Avoyan <michael.avoyan.dev@gmail.com>
2023-12-06 15:49:57 +0200

    cleanup

--- packages/sdk/src/test/entities/VCLJwt.test.ts
+++ packages/sdk/src/test/entities/VCLJwt.test.ts
@@ -1,6 +1,5 @@
 import VCLJwt, { SignedJWT } from "../../api/entities/VCLJwt";
 import "../../impl/extensions/StringExtensions";
-import assert from "assert"
 
     var subject: VCLJwt
     let jwtStr = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InNvbWVfa2lkIn0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.lGqyFj2RpmostxLvCcMFym9kWM0bzQcty9a81EdzrhM"
@@ -18,7 +17,7 @@ import assert from "assert"
             expect(subject.kid == "").toBeTruthy()
         })
 
-        test("valid jwt", async () => {
+        test("valid jwt with kid", async () => {
             subject = VCLJwt.fromEncodedJwt(jwtStr)
 
             expect(JSON.stringify(subject.header) == "{\"alg\":\"HS256\",\"typ\":\"JWT\",\"kid\":\"some_kid\"}").toBeTruthy()
@@ -29,4 +28,14 @@ import assert from "assert"
             expect(subject.kid == "some_kid").toBeTruthy()
         })
 
+        test("valid jwt with jwk", async () => {
+            subject = VCLJwt.fromEncodedJwt(jwtStr)
+
+            expect(JSON.stringify(subject.header) == "{\"alg\":\"HS256\",\"typ\":\"JWT\",\"kid\":\"some_kid\"}").toBeTruthy()
+            expect(JSON.stringify(subject.payload) == "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}").toBeTruthy()
+            expect(subject.signature == "lGqyFj2RpmostxLvCcMFym9kWM0bzQcty9a81EdzrhM").toBeTruthy()
+            expect(subject.encodedJwt == jwtStr).toBeTruthy()
+            expect(JSON.stringify(subject.jwk) == "{}").toBeTruthy()
+            expect(subject.kid == "some_kid").toBeTruthy()
+        })
     })
