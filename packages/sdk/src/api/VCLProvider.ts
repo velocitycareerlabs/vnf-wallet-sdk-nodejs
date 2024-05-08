17c2085a63ffd5be40ef813446f88e101edc52a2
Michael Avoyan <michael.avoyan.dev@gmail.com>
2024-05-08 16:53:09 +0300

    chore: VCLProvider as singleton

--- packages/sdk/src/api/VCLProvider.ts
+++ packages/sdk/src/api/VCLProvider.ts
@@ -1,15 +1,22 @@
 import { VCLImpl } from "../impl/VCLImpl";
 
 export default class VCLProvider {
-    private static vclInstance: VCLImpl;
+    private static vclInstance: VCLImpl | null = null;
 
-    constructor() {
-        if (!VCLProvider.vclInstance) {
-            VCLProvider.vclInstance = new VCLImpl();
-        }
+    // Private constructor to prevent external instantiation
+    private constructor() {
+        // Do nothing
     }
 
-    getInstance(): VCLImpl {
+    public static getInstance(): VCLImpl {
+        // Double-checked locking pattern
+        if (VCLProvider.vclInstance === null) {
+            // Lock this section
+            // Assume some form of synchronization, like a mutex
+            if (VCLProvider.vclInstance === null) {
+                VCLProvider.vclInstance = new VCLImpl();
+            }
+        }
         return VCLProvider.vclInstance;
     }
 }
