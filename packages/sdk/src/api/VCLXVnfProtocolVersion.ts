e48d3909ea99f30c1dbde4539035532898b5eba4
Michael Avoyan <michael.avoyan.dev@gmail.com>
2024-06-03 15:46:26 +0300

    v0.9.6

--- packages/sdk/src/api/VCLXVnfProtocolVersion.ts
+++ packages/sdk/src/api/VCLXVnfProtocolVersion.ts
@@ -1,4 +1,13 @@
-export enum VCLXVnfProtocolVersion {
+/**
+ * Created by Michael Avoyan on 03/06/2024.
+ *
+ * Copyright 2022 Velocity Career Labs inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+enum VCLXVnfProtocolVersion {
     XVnfProtocolVersion1 = "1.0",
     XVnfProtocolVersion2 = "2.0"
 }
+
+export default VCLXVnfProtocolVersion;
