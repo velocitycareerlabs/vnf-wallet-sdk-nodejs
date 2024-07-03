a2efd02b9363cae29716bbb5a2b38be5f4d85c30
Michael Avoyan <michael.avoyan.dev@gmail.com>
2024-07-03 13:10:57 +0300

    submit presentation

--- packages/sample-server/src/controllers/submitPresentation.ts
+++ packages/sample-server/src/controllers/submitPresentation.ts
@@ -5,20 +5,36 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { VCLPresentationSubmission } from "@velocitycareerlabs/vnf-nodejs-wallet-sdk/src";
+import {
+    VCLDeepLink, VCLDidJwk,
+    VCLJwt,
+    VCLPresentationRequest,
+    VCLVerifiedProfile
+} from "@velocitycareerlabs/vnf-nodejs-wallet-sdk/src";
 
 export async function submitPresentation(req, reply) {
     try {
+        const presentationRequestJson = req.body.presentationRequest;
+        const verifiableCredentials = req.body.verifiableCredentials
+        const presentationRequest = new VCLPresentationRequest(
+            VCLJwt.fromEncodedJwt(presentationRequestJson.jwt.encodedJwt),
+            new VCLVerifiedProfile(presentationRequestJson.verifiedProfile.payload),
+            new VCLDeepLink(presentationRequestJson.deepLink.value),
+            null,
+            VCLDidJwk.fromJSON(presentationRequestJson.didJwk),
+            req.body.remoteCryptoServicesToken
+        )
         const presentationSubmission = new VCLPresentationSubmission(
-            req.body.presentationRequest,
-            req.body.verifiableCredentials
+            presentationRequest,
+            verifiableCredentials
         )
         const submissionResult = await req.vclSdk.submitPresentation(presentationSubmission);
         reply.send(submissionResult);
     } catch (e: any) {
         reply.code(500).send({
             statusCode: "500",
-            error: e,
-            message: "Failed to submit presentation",
+            error: "Failed to submit presentation",
+            message: e.message ?? JSON.stringify(e),
         });
     }
 }
