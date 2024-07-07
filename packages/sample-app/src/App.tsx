d987a30a61a670f20d42770bc6de9d191673cda7
Michael Avoyan <michael.avoyan.dev@gmail.com>
2024-07-07 17:54:43 +0300

    sample app

---
+++ packages/sample-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Menu from "./Menu";
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <Menu />
+        </header>
+      </div>
+  );
+}
+
+export default App;
