985e330a5ef9851d447ff918ba82504afc51f3fa
Michael Avoyan <michael.avoyan.dev@gmail.com>
2024-07-11 17:00:44 +0300

    refactor cont

--- packages/sample-server/src/crypto-services/fetchers/GenerateSignedJwtFetcher.ts
+++ packages/sample-server/src/crypto-services/fetchers/GenerateSignedJwtFetcher.ts
@@ -8,12 +8,7 @@
 import { getJwtSignServiceUrl } from "./Urls";
 import { CurrentEnvironment } from "../../GlobalConfig";
 import fetcher from "./Fetcher";
-import {
-    VCLDidJwk,
-    VCLJwtDescriptor,
-    Dictionary,
-    Nullish
-} from "@velocitycareerlabs/vnf-nodejs-wallet-sdk/src";
+import { Dictionary, Nullish, VCLDidJwk, VCLJwtDescriptor } from "@velocitycareerlabs/vnf-nodejs-wallet-sdk/src";
 
 export async function generateSignedJwtFetcher(
     jwtDescriptor: VCLJwtDescriptor,
@@ -37,40 +32,21 @@ function generateJwtPayloadToSign(
     nonce: Nullish<string>,
     didJwk: VCLDidJwk
 ): Dictionary<any> {
-    const retVal: Dictionary<any> = {};
-    const header: Dictionary<any> = {};
-    const options: Dictionary<any> = {};
     const payload: Dictionary<any> = jwtDescriptor.payload ? { ...jwtDescriptor.payload } : {};
-
-    header[CodingKeys.KeyJwk] = didJwk.publicJwk.valueJson;
-    header[CodingKeys.KeyKid] = didJwk.kid;
-
-    options[CodingKeys.KeyKeyId] = didJwk.keyId;
-
-    payload[CodingKeys.KeyNonce] = nonce;
-    payload[CodingKeys.KeyAud] = jwtDescriptor.aud;
-    payload[CodingKeys.KeyJti] = jwtDescriptor.jti;
-    payload[CodingKeys.KeyIss] = jwtDescriptor.iss;
-
-    retVal[CodingKeys.KeyHeader] = header;
-    retVal[CodingKeys.KeyOptions] = options;
-    retVal[CodingKeys.KeyPayload] = payload;
-
-    return retVal;
+    return {
+        header: {
+            jwk: didJwk.publicJwk.valueJson,
+            kid: didJwk.kid
+        },
+        payload: {
+            ...payload,
+            nonce: nonce,
+            aud: jwtDescriptor.aud,
+            jti: jwtDescriptor.jti,
+            iss: jwtDescriptor.iss
+        },
+        options: {
+            keyId: didJwk.keyId
+        }
+    };
 }
-
-const CodingKeys = {
-    KeyKeyId: "keyId",
-    KeyKid: "kid",
-    KeyJwk: "jwk",
-    KeyIss: "iss",
-    KeyAud: "aud",
-    KeyJti: "jti",
-    KeyNonce: "nonce",
-
-    KeyHeader: "header",
-    KeyOptions: "options",
-    KeyPayload: "payload",
-
-    KeyCompactJwt: "compactJwt"
-};
